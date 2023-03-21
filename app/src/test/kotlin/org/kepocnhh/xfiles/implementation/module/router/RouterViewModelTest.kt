7301837c7a96d607adb9574da9142e3333983e90
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-21 20:39:50 +0300

    dispatch timeout ms

--- app/src/test/kotlin/org/kepocnhh/xfiles/implementation/module/router/RouterViewModelTest.kt
+++ app/src/test/kotlin/org/kepocnhh/xfiles/implementation/module/router/RouterViewModelTest.kt
@@ -1,34 +1,19 @@
 package org.kepocnhh.xfiles.implementation.module.router
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onEmpty
-import kotlinx.coroutines.flow.onSubscription
 import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
 import kotlinx.coroutines.withTimeout
 import org.junit.Test
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
-import org.junit.BeforeClass
-import org.junit.Rule
 import org.kepocnhh.xfiles.implementation.provider.mockInjection
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -99,28 +84,26 @@ internal class RouterViewModelTest {
         }
     }
 
-    @Test(timeout = 10_000)
+    @Test
     fun bar() {
         val injection = mockInjection()
         val viewModel = RouterViewModel(injection)
-        runTest {
-            withTimeout(10_000) {
-                viewModel.state
-                    .withIndex()
-                    .onEach { (index, value) ->
-                        when (index) {
-                            0 -> {
-                                assertNull(value)
-                                viewModel.requestFile()
-                            }
-                            1 -> {
-                                val exists = checkNotNull(value)
-                                assertFalse("File exists!", exists)
-                            }
-                            else -> TODO()
+        runTest(dispatchTimeoutMs = 10_000) {
+            viewModel.state
+                .withIndex()
+                .onEach { (index, value) ->
+                    when (index) {
+                        0 -> {
+                            assertNull(value)
+                            viewModel.requestFile()
                         }
-                    }.take(2).collect()
-            }
+                        1 -> {
+                            val exists = checkNotNull(value)
+                            assertFalse("File exists!", exists)
+                        }
+                        else -> TODO()
+                    }
+                }.take(2).collect()
         }
     }
 }
