64e125fc9ccd467410b1288aadaebf0c0ee3fb92
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-23 20:11:01 +0300

    clicks

--- app/src/main/kotlin/org/kepocnhh/xfiles/presentation/module/onfile/NewItemScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/presentation/module/onfile/NewItemScreen.kt
@@ -1,16 +1,6 @@
 package org.kepocnhh.xfiles.presentation.module.onfile
 
-import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.hoverable
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,127 +15,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusProperties
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.InputMode
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.key.type
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalInputModeManager
-import androidx.compose.ui.semantics.clearAndSetSemantics
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.onLongClick
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.center
-import androidx.compose.ui.unit.toOffset
-import kotlinx.coroutines.launch
 import org.kepocnhh.xfiles.App
-import org.kepocnhh.xfiles.presentation.util.androidx.compose.foundation.catchClicks
-import org.kepocnhh.xfiles.presentation.util.androidx.compose.foundation.combinedClickable
+import org.kepocnhh.xfiles.presentation.util.androidx.compose.foundation.clicks
 
 @Composable
 private fun KeyboardRow(buttons: Set<Char>, onClick: (Char) -> Unit) {
     Row(
-        Modifier
+        modifier = Modifier
             .fillMaxWidth()
-            .height(App.Theme.dimensions.sizes.xl)) {
+            .height(App.Theme.dimensions.sizes.xl),
+    ) {
         for (char in buttons) {
-//            val scope = rememberCoroutineScope()
-            val source = remember { MutableInteractionSource() }
-            val indication = LocalIndication.current
-            val state = source.collectIsDraggedAsState()
-//            val inputModeManager = LocalInputModeManager.current
-//            val enabled = true
-//            val offset = remember { mutableStateOf(Offset.Zero) }
-//            val interactions = remember { mutableMapOf<Key, PressInteraction.Press>() }
             BasicText(
                 modifier = Modifier
                     .fillMaxHeight()
                     .weight(1f)
-//                    .clearAndSetSemantics {
-//                        onClick {
-//                            println("on click: $char")
-//                            onClick(char)
-//                            true
-//                        }
-//                        onLongClick {
-//                            println("on long click: $char")
-//                            char.takeIf { it.isLowerCase() }
-//                                ?.uppercaseChar()
-//                                ?.takeIf { it.isUpperCase() }
-//                                ?.also(onClick) ?: onClick(char)
-//                            true
-//                        }
-//                    }
-//                    .pointerInput(source) {
-//                        offset.value = size.center.toOffset()
-//                    }
-//                    .onKeyEvent { event ->
-//                        when (event.type) {
-//                            KeyEventType.KeyDown -> {
-//                                val contains = interactions.containsKey(event.key)
-//                                val it = PressInteraction.Press(offset.value).also {
-//                                    interactions[event.key] = it
-//                                }
-//                                scope.launch { source.emit(it) }
-//                                !contains
-//                            }
-//                            KeyEventType.KeyUp -> {
-//                                interactions.remove(event.key)?.also {
-//                                    scope.launch { source.emit(PressInteraction.Release(it)) }
-//                                }
-//                                true
-//                            }
-//                            else -> true
-//                        }
-//                    }
-//                    .indication(interactionSource = source, indication)
-//                    .hoverable(enabled = enabled, interactionSource = source)
-//                    .focusProperties { canFocus = inputModeManager.inputMode != InputMode.Touch }
-//                    .focusable(enabled = enabled, interactionSource = source)
-//                    .pointerInput(null) {
-//                        detectTapGestures(
-//                            onLongPress = { _ ->
-//                                println("on long press: $char")
-//                                char.takeIf { it.isLowerCase() }
-//                                    ?.uppercaseChar()
-//                                    ?.takeIf { it.isUpperCase() }
-//                                    ?.also(onClick) ?: onClick(char)
-//                            },
-//                            onTap = { _ ->
-//                                onClick(char)
-//                            },
-//                        )
-//                    }
-                    .combinedClickable(
-                        interactionSource = source,
-                        indication = indication,
+                    .clicks(
                         onClick = {
-                            println("on click: $char")
                             onClick(char)
                         },
                         onLongClick = {
-                            println("on long click: $char")
-                            char
-                                .takeIf { it.isLowerCase() }
-                                ?.uppercaseChar()
-                                ?.takeIf { it.isUpperCase() }
-                                ?.also(onClick)
-                        }
+                            if (char.isLowerCase()) {
+                                val upper = char.uppercaseChar()
+                                if (upper.isUpperCase()) {
+                                    onClick(upper)
+                                }
+                            }
+                        },
                     )
-//                    .clickable {
-//                        onClick(char)
-//                    }
-//                    .catchClicks()
                     .wrapContentHeight(),
                 text = "$char",
                 style = TextStyle(
