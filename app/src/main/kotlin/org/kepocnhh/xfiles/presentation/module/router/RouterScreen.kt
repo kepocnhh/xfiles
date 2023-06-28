12ba5d766df581af34d81a79d265c711b4e71c68
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-28 15:36:20 +0300

    animations

--- app/src/main/kotlin/org/kepocnhh/xfiles/presentation/module/router/RouterScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/presentation/module/router/RouterScreen.kt
@@ -1,9 +1,17 @@
 package org.kepocnhh.xfiles.presentation.module.router
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.kepocnhh.xfiles.App
 import org.kepocnhh.xfiles.implementation.module.router.RouterViewModel
@@ -19,21 +27,42 @@ internal fun RouterScreen() {
                 viewModel.requestState()
             }
             else -> {
-                when (state.exists) {
-                    true -> {
-                        OnFileScreen(
-                            onDelete = {
-                                viewModel.requestState()
-                            }
-                        )
-                    }
-                    false -> {
-                        NoFileScreen(
-                            onCreate = {
-                                viewModel.requestState()
-                            }
-                        )
-                    }
+                AnimatedVisibility(
+                    visible = state.exists,
+                    enter = slideInHorizontally()
+                            + expandHorizontally(expandFrom = Alignment.End)
+                            + fadeIn(),
+                    exit = slideOutHorizontally(targetOffsetX = { it })
+                            + shrinkHorizontally()
+                            + fadeOut(),
+                ) {
+                    OnFileScreen(
+                        onDelete = {
+                            viewModel.requestState()
+                        }
+                    )
+                }
+//                if (!state.exists) {
+//                    NoFileScreen(
+//                        onCreate = {
+//                            viewModel.requestState()
+//                        }
+//                    )
+//                }
+                AnimatedVisibility(
+                    visible = !state.exists,
+                    enter = slideInHorizontally()
+                            + expandHorizontally(expandFrom = Alignment.End)
+                            + fadeIn(),
+                    exit = slideOutHorizontally(targetOffsetX = { it })
+                            + shrinkHorizontally()
+                            + fadeOut(),
+                ) {
+                    NoFileScreen(
+                        onCreate = {
+                            viewModel.requestState()
+                        }
+                    )
                 }
             }
         }
