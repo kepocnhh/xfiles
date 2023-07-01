7954efd620f13e2b0588b33a542739228a1be462
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-01 18:38:58 +0300

    signature

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterViewModel.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterViewModel.kt
@@ -21,8 +21,10 @@ import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.SecureRandom
 import java.security.Security
+import java.security.Signature
 import java.security.spec.AlgorithmParameterSpec
 import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
@@ -86,8 +88,9 @@ internal class EnterViewModel : ViewModel() {
         viewModelScope.launch {
             _exists.value = null
             withContext(Dispatchers.IO) {
+//                error(Security.getAlgorithms("Signature").toList())
+                // [NONEWITHDSA, SHA384WITHECDSA, SHA224WITHDSA, SHA384WITHRSA/PSS, SHA256WITHRSA, MD5WITHRSA, ED25519, SHA1WITHRSA, SHA256WITHRSA/PSS, SHA512WITHRSA, SHA512WITHRSA/PSS, SHA256WITHDSA, SHA1WITHECDSA, NONEWITHECDSA, SHA224WITHRSA, NONEWITHRSA, SHA256WITHECDSA, SHA224WITHECDSA, SHA384WITHRSA, SHA512WITHECDSA, SHA1WITHRSA/PSS, SHA224WITHRSA/PSS, SHA1WITHDSA]
                 val random = getSecureRandom()
-                val factory = SecretKeyFactory.getInstance(algorithm)
                 val sym1 = KeyMeta(
                     salt = ByteArray(32).also(random::nextBytes),
                     iv = ByteArray(16).also(random::nextBytes)
@@ -118,13 +121,19 @@ internal class EnterViewModel : ViewModel() {
                     val encrypted = cipher.encrypt(pair.public, password)
                     parent.resolve("sym2.enc").writeBytes(encrypted)
                 }
+                val decrypted = "{}".toByteArray()
                 Cipher.getInstance(algorithm).also { cipher ->
                     val chars = password.base64().toCharArray()
                     val key = generateSecret(cipher.algorithm, chars, sym2.salt)
                     val params = IvParameterSpec(sym2.iv)
-                    val decrypted = "{}"
-                    val encrypted = cipher.encrypt(key, params, decrypted.toByteArray())
+                    val encrypted = cipher.encrypt(key, params, decrypted)
                     parent.resolve("db.json.enc").writeBytes(encrypted)
+                }
+//                Signature.getInstance("SHA256WithDSA").also { signature ->
+                Signature.getInstance("SHA256WITHRSA").also { signature ->
+                    signature.initSign(pair.private, random)
+                    signature.update(decrypted)
+                    parent.resolve("db.json.sig").writeBytes(signature.sign())
                 }
             }
             _exists.value = true // todo
@@ -184,25 +193,47 @@ internal class EnterViewModel : ViewModel() {
                         iv = json.getString("iv").let { Base64.decode(it, Base64.DEFAULT) },
                     )
                 }
-                val private = Cipher.getInstance(algorithm).let { cipher ->
-                    val encrypted = JSONObject(parent.resolve("asym.json").readText()).let { json ->
-                        json.getString("private").let { Base64.decode(it, Base64.DEFAULT) }
+                val (public, private) = Cipher.getInstance(algorithm).let { cipher ->
+                    val (public, encrypted) = JSONObject(parent.resolve("asym.json").readText()).let { json ->
+                        json.getString("public").let {
+                            Base64.decode(it, Base64.DEFAULT)
+                        } to json.getString("private").let {
+                            Base64.decode(it, Base64.DEFAULT)
+                        }
                     }
                     val key = generateSecret(algorithm = cipher.algorithm, chars = pin.toCharArray(), salt = sym1.salt)
                     val params = IvParameterSpec(sym1.iv)
                     val decrypted = cipher.decrypt(key, params, encrypted)
-                    KeyFactory.getInstance("RSA")
-                        .generatePrivate(PKCS8EncodedKeySpec(decrypted))
+                    KeyFactory.getInstance("RSA").let { factory ->
+                        factory.generatePublic(
+                            X509EncodedKeySpec(public)
+                        ) to factory.generatePrivate(
+                            PKCS8EncodedKeySpec(decrypted)
+                        )
+                    }
                 }
-//                val sym2 = JSONObject(parent.resolve("sym2.json").readText()).let { json ->
-//                    KeyMeta(
-//                        salt = json.getString("salt").let { Base64.decode(it, Base64.DEFAULT) },
-//                        iv = json.getString("iv").let { Base64.decode(it, Base64.DEFAULT) },
-//                    )
-//                }
+                val sym2 = JSONObject(parent.resolve("sym2.json").readText()).let { json ->
+                    KeyMeta(
+                        salt = json.getString("salt").let { Base64.decode(it, Base64.DEFAULT) },
+                        iv = json.getString("iv").let { Base64.decode(it, Base64.DEFAULT) },
+                    )
+                }
                 val password = Cipher.getInstance("RSA/ECB/PKCS1Padding").let { cipher ->
                     val encrypted = parent.resolve("sym2.enc").readBytes()
                     cipher.decrypt(private, encrypted)
+                }
+                val decrypted = Cipher.getInstance(algorithm).let { cipher ->
+                    val chars = password.base64().toCharArray()
+                    val key = generateSecret(algorithm = cipher.algorithm, chars = chars, salt = sym2.salt)
+                    val params = IvParameterSpec(sym2.iv)
+                    val encrypted = parent.resolve("db.json.enc").readBytes()
+                    cipher.decrypt(key, params, encrypted)
+                }
+//                Signature.getInstance("SHA256WithDSA").also { signature ->
+                Signature.getInstance("SHA256WITHRSA").also { signature ->
+                    signature.initVerify(public)
+                    signature.update(decrypted)
+                    signature.verify(parent.resolve("db.json.sig").readBytes())
                 }
                 Broadcast.OnUnlock
             }
