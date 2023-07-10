4f8c755914342828fc52f7d265962ce775e13eb4
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-10 22:00:30 +0300

    swipe

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/unlocked/UnlockedScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/unlocked/UnlockedScreen.kt
@@ -2,6 +2,9 @@ package org.kepocnhh.xfiles.module.unlocked
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -9,6 +12,9 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,7 +23,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -30,6 +38,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import javax.crypto.SecretKey
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.unit.IntOffset
+import kotlin.math.roundToInt
 
 internal object UnlockedScreen {
     sealed interface Broadcast {
@@ -39,9 +51,44 @@ internal object UnlockedScreen {
 
 @Composable
 private fun Data(values: Map<String, String>) {
-    Column(verticalArrangement = Arrangement.Center) {
-        values.forEach { (k, v) ->
-            BasicText(text = "$k: $v")
+    LazyColumn {
+        items(values.keys.toList()) { name ->
+            val value = values[name] ?: TODO()
+            val draggableState = remember { mutableStateOf(false) }
+            val animatable = remember { Animatable(0f) }
+            val offsetXState = remember { mutableStateOf(0f) }
+            LaunchedEffect(draggableState.value) {
+                if (draggableState.value) {
+                    animatable.stop()
+                } else {
+                    animatable.snapTo(offsetXState.value)
+                    animatable.animateTo(0f)
+                }
+            }
+            if (!draggableState.value) {
+                if (animatable.isRunning) {
+                    offsetXState.value = animatable.value
+                }
+            }
+            BasicText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .offset { IntOffset(offsetXState.value.roundToInt() / 2, 0) }
+                    .draggable(
+                        orientation = Orientation.Horizontal,
+                        state = rememberDraggableState { delta ->
+                            offsetXState.value += delta
+                        },
+                        onDragStarted = {
+                            draggableState.value = true
+                        },
+                        onDragStopped = {
+                            draggableState.value = false
+                        },
+                    ),
+                text = "$name: $value"
+            )
         }
     }
 }
