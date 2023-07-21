bc152c1a8a1a96c21cc6419ac9219db7c36ce4c0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-21 14:03:09 +0300

    delete obsolete

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterScreen.kt
@@ -1,21 +1,11 @@
 package org.kepocnhh.xfiles.module.enter
 
 import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
@@ -24,11 +14,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
@@ -36,70 +24,11 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.kepocnhh.xfiles.App
 import org.kepocnhh.xfiles.util.android.showToast
 import org.kepocnhh.xfiles.util.compose.PinPad
 import sp.ax.jc.dialogs.Dialog
 import javax.crypto.SecretKey
-import kotlin.time.Duration.Companion.seconds
-
-@Composable
-private fun Top(
-    exists: Boolean?,
-    onDelete: () -> Unit,
-) {
-    val durationMillis = 250
-//    val durationMillis = 2_000
-    AnimatedVisibility(
-        visible = exists == null,
-        enter = fadeIn(tween(durationMillis)),
-        exit = fadeOut(tween(durationMillis)),
-    ) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .background(Color.Yellow)) {
-            println("loading...")
-            BasicText(modifier = Modifier.align(Alignment.Center), text = "loading...")
-        }
-    }
-    AnimatedVisibility(
-        visible = exists == true,
-        enter = slideInHorizontally(tween(durationMillis), initialOffsetX = { it })
-        + fadeIn(tween(durationMillis)),
-        exit = slideOutHorizontally(tween(durationMillis), targetOffsetX = { it })
-        + fadeOut(tween(durationMillis)),
-    ) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .background(Color.Red)) {
-            Column(modifier = Modifier.align(Alignment.Center)) {
-                BasicText(modifier = Modifier.padding(8.dp), text = "exists")
-                BasicText(modifier = Modifier
-                    .clickable { onDelete() }
-                    .padding(8.dp), text = "delete")
-            }
-        }
-    }
-    AnimatedVisibility(
-        visible = exists == false,
-        enter = slideInHorizontally(tween(durationMillis), initialOffsetX = { -it }),
-        exit = slideOutHorizontally(tween(durationMillis), targetOffsetX = { -it }),
-    ) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .background(Color.Green)) {
-            BasicText(modifier = Modifier.align(Alignment.Center), text = "none")
-        }
-    }
-}
 
 internal object EnterScreen {
     sealed interface Broadcast {
@@ -111,12 +40,17 @@ internal object EnterScreen {
 internal fun EnterScreen(broadcast: (EnterScreen.Broadcast) -> Unit) {
     when (LocalConfiguration.current.orientation) {
         Configuration.ORIENTATION_LANDSCAPE -> {
-            TODO("EnterScreen:LANDSCAPE")
+            EnterScreenLandscape(broadcast = broadcast)
         }
         else -> {
             EnterScreenPortrait(broadcast = broadcast)
         }
     }
+}
+
+@Composable
+private fun EnterScreenLandscape(broadcast: (EnterScreen.Broadcast) -> Unit) {
+    TODO()
 }
 
 @Composable
@@ -230,101 +164,5 @@ private fun EnterScreenPortrait(broadcast: (EnterScreen.Broadcast) -> Unit) {
                 deleteDialog = true
             }
         )
-    }
-}
-
-@Composable
-private fun EnterScreenOld(broadcast: (EnterScreen.Broadcast) -> Unit) {
-    val context = LocalContext.current
-    val viewModel = viewModel<EnterViewModel>()
-    val pinState = remember { mutableStateOf("") }
-    LaunchedEffect(Unit) {
-        viewModel.broadcast.collect { broadcast ->
-            when (broadcast) {
-                is EnterViewModel.Broadcast.OnUnlock -> {
-                    broadcast(EnterScreen.Broadcast.Unlock(broadcast.key))
-                }
-                EnterViewModel.Broadcast.OnUnlockError -> {
-                    pinState.value = ""
-                    context.showToast("on unlock error...")
-                    // todo
-                }
-            }
-        }
-    }
-    val exists = viewModel.exists.collectAsState(null)
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            val deleteDialog = remember { mutableStateOf(false) }
-            Top(
-                exists = exists.value,
-                onDelete = {
-                    deleteDialog.value = true
-                }
-            )
-            if (deleteDialog.value) {
-                Dialog(
-                    "ok" to {
-                        viewModel.deleteFile()
-                        deleteDialog.value = false
-                    },
-                    message = "delete?",
-                    onDismissRequest = { deleteDialog.value = false }
-                )
-            }
-            println("exists: ${exists.value}")
-            LaunchedEffect(Unit) {
-                if (exists.value == null) {
-                    viewModel.requestFile()
-                }
-            }
-        }
-        LaunchedEffect(pinState.value) {
-            if (pinState.value.length == 4) {
-//                when (exists.value) {
-//                    true -> {
-//                        viewModel.unlockFile(context.cacheDir, pinState.value)
-//                    }
-//                    false -> {
-//                        viewModel.createNewFile(context.cacheDir, pinState.value)
-//                    }
-//                    null -> {
-//                        // noop
-//                    }
-//                }
-            }
-        }
-        BasicText(modifier = Modifier
-            .clickable { pinState.value = "" }
-            .padding(8.dp), text = "x")
-        BasicText(text = pinState.value)
-        PinPad(
-            modifier = Modifier
-                .fillMaxWidth(),
-            rowHeight = 64.dp,
-            textStyle = TextStyle(
-                textAlign = TextAlign.Center,
-                color = Color.Black,
-                fontSize = 24.sp,
-            ),
-            onClick = { char ->
-                pinState.value += char
-            },
-            onDelete = {
-                // todo
-            },
-            onDeleteLong = {
-                // todo
-            },
-        )
-        Spacer(modifier = Modifier.height(128.dp))
     }
 }
