60170ace45b5690e2edb33112ff26cb24ec3a14a
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-25 16:22:07 +0300

    add item portrait

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/unlocked/AddItemScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/unlocked/AddItemScreen.kt
@@ -3,6 +3,7 @@ package org.kepocnhh.xfiles.module.unlocked
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,6 +11,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,15 +20,18 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import org.kepocnhh.xfiles.App
 import org.kepocnhh.xfiles.util.compose.Keyboard
 import org.kepocnhh.xfiles.util.compose.TextFocused
 import sp.ax.jc.dialogs.Dialog
@@ -52,60 +57,90 @@ internal fun AddItemScreen(
     BackHandler {
         onCancel()
     }
+    val keyState = remember { mutableStateOf("") }
+    val valueState = remember { mutableStateOf("") }
+    val focusedState = remember { mutableStateOf(Focused.KEY) }
+    val textDialogState = remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    if (textDialogState.value) {
+        Dialog(
+            "paste" to {
+                val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = clipboardManager.primaryClip
+                val text = clip?.getFirstTextOrNull()
+                if (text != null) {
+                    when (focusedState.value) {
+                        Focused.KEY -> {
+                            keyState.value = text
+                        }
+                        Focused.VALUE -> {
+                            valueState.value = text
+                        }
+                    }
+                }
+                textDialogState.value = false
+            },
+            "clear" to {
+                when (focusedState.value) {
+                    Focused.KEY -> {
+                        keyState.value = ""
+                    }
+                    Focused.VALUE -> {
+                        valueState.value = ""
+                    }
+                }
+                textDialogState.value = false
+            },
+            message = "?",
+            onDismissRequest = { textDialogState.value = false },
+        )
+    }
+    when (val orientation = LocalConfiguration.current.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            TODO()
+        }
+        Configuration.ORIENTATION_PORTRAIT -> {
+            AddItemScreenPortrait(
+                keys = keys,
+                keyState = keyState,
+                valueState = valueState,
+                focusedState = focusedState,
+                textDialogState = textDialogState,
+                onAdd = onAdd,
+            )
+        }
+        else -> error("Orientation $orientation is not supported!")
+    }
+}
+
+@Composable
+private fun AddItemScreenPortrait(
+    keys: Set<String>,
+    keyState: MutableState<String>,
+    valueState: MutableState<String>,
+    focusedState: MutableState<Focused>,
+    textDialogState: MutableState<Boolean>,
+    onAdd: (String, String) -> Unit,
+) {
     Box(
         modifier = Modifier
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null,
                 onClick = {},
-            )
+            ) // todo clickable?
             .fillMaxSize()
-            .background(Color.White),
+            .background(App.Theme.colors.background)
+            .padding(
+                top = App.Theme.dimensions.insets.calculateTopPadding(),
+                bottom = App.Theme.dimensions.insets.calculateBottomPadding(),
+            ),
     ) {
-        val keyState = remember { mutableStateOf("") }
-        val valueState = remember { mutableStateOf("") }
-        val focusedState = remember { mutableStateOf(Focused.KEY) }
-        val textDialogState = remember { mutableStateOf(false) }
-        val context = LocalContext.current
-        if (textDialogState.value) {
-            Dialog(
-                "paste" to {
-                    val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                    val clip = clipboardManager.primaryClip
-                    val text = clip?.getFirstTextOrNull()
-                    if (text != null) {
-                        when (focusedState.value) {
-                            Focused.KEY -> {
-                                keyState.value = text
-                            }
-                            Focused.VALUE -> {
-                                valueState.value = text
-                            }
-                        }
-                    }
-                    textDialogState.value = false
-                },
-                "clear" to {
-                    when (focusedState.value) {
-                        Focused.KEY -> {
-                            keyState.value = ""
-                        }
-                        Focused.VALUE -> {
-                            valueState.value = ""
-                        }
-                    }
-                    textDialogState.value = false
-                },
-                message = "?",
-                onDismissRequest = { textDialogState.value = false },
-            )
-        }
         Column(
-            modifier = Modifier
-                .padding(top = 64.dp)
+            modifier = Modifier.fillMaxWidth(),
         ) {
             BasicText(
-                modifier = Modifier,
+                modifier = Modifier.padding(start = 16.dp),
                 text = "key:",
             )
             TextFocused(
@@ -133,8 +168,9 @@ internal fun AddItemScreen(
                 },
                 focused = focusedState.value == Focused.KEY,
             )
+            Spacer(modifier = Modifier.height(16.dp))
             BasicText(
-                modifier = Modifier,
+                modifier = Modifier.padding(start = 16.dp),
                 text = "value:",
             )
             TextFocused(
@@ -190,8 +226,7 @@ internal fun AddItemScreen(
         Keyboard(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 64.dp),
+                .align(Alignment.BottomCenter),
             onClick = {
                 when (focusedState.value) {
                     Focused.KEY -> keyState.value += it
@@ -213,6 +248,5 @@ internal fun AddItemScreen(
                 }
             }
         )
-        // todo
     }
 }
