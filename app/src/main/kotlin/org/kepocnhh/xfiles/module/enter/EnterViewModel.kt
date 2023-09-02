6680eff24f7430b336bf4eb664e6b96b4c704f6d
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-02 18:29:17 +0300

    dsa...

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterViewModel.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/EnterViewModel.kt
@@ -22,6 +22,7 @@ import java.math.BigInteger
 import java.security.KeyFactory
 import java.security.KeyPairGenerator
 import java.security.MessageDigest
+import java.security.SecureRandom
 import java.security.Security
 import java.security.Signature
 import java.security.interfaces.DSAParams
@@ -69,10 +70,121 @@ internal class EnterViewModel(private val injection: Injection) : AbstractViewMo
         return md.digest(pin.toByteArray())
     }
 
+    private fun getP(
+        q: BigInteger,
+        L: Int,
+        certainty: Int,
+        random: SecureRandom,
+    ): BigInteger {
+        check(q.isProbablePrime(certainty))
+        // (p - 1) % q == 0
+        val m = BigInteger.ZERO
+        var index = 0
+        while (true) {
+            index++
+//            if (index > 10) TODO()
+            println("$index]")
+            // a / b = c
+            // a % b = m
+            // b * c + m = a
+            // (p - 1) / q = c
+            // (p - 1) % q = m
+            // p - 1 = c * q + m
+            // q * c + m = (p - 1)
+            // q * c + m + 1 = p
+            // m = 0 // q * c + 1 = p
+//            val c = BigInteger(L, certainty, random)
+            // p = q * c + m + 1
+            // q * c = p - m - 1
+            // c = (p - m - 1) / q
+//            val cLen = ((BigInteger(L, certainty, random) - BigInteger.ONE - m) / q).bitLength()
+//            val c = BigInteger(cLen, certainty, random)
+//            println("\ttmp(c): bit length: ${c.bitLength()}")
+//            val p = q * c + m + BigInteger.ONE
+//            val w = ByteArray(L / 8)
+//            val p = BigInteger(1, w)
+//            val l1 = BigInteger(L, certainty, random)
+//            val l2 = l1.subtract(BigInteger.ONE) // l2 = l1 - 1
+//            val p = l1.subtract(l2.remainder(q)) // p = l1 - (l2 % q) // p = l1 - ((l1 - 1) % q)
+//            val w = ByteArray(L / 8)
+//            val x = BigInteger(1, w)
+//            val c = x.mod(q.shiftLeft(1))
+//            val p = x.subtract(c.subtract(BigInteger.ONE))
+            val l1 = BigInteger(L, certainty, random)
+//            val lm = l1.subtract(BigInteger.ONE).mod(q) // lm = (l1 - 1) % q
+            val ld = l1.subtract(BigInteger.ONE).div(q) // ld = (l1 - 1) / q
+            // ld * q = l1 - 1
+            // ld * q + 1 = p
+//            val p = ld * l1.subtract(BigInteger.ONE) // p = ld * (l1 - 1)
+            val p = ld.multiply(q).add(BigInteger.ONE) // p = ld * q + 1
+//            val p = BigInteger(L, certainty, random)
+            println("\ttmp(p): $p")
+            println("\ttmp(p): bit length: ${p.bitLength()}")
+            if (p.bitLength() != L) continue
+            println("\tp-1: ${p.subtract(BigInteger.ONE)}")
+            println("\t(p-1)%q: ${p.subtract(BigInteger.ONE).mod(q)}")
+            if (p.subtract(BigInteger.ONE).mod(q) != m) continue
+            if (p.isProbablePrime(certainty)) return p
+//            if (p.subtract(BigInteger.ONE).mod(q) == BigInteger.ZERO) {
+//                return p
+//            }
+        }
+    }
+
+    private fun getG(
+        p: BigInteger,
+        q: BigInteger,
+        random: SecureRandom,
+    ): BigInteger {
+        // g = h^((p-1)/q) mod p
+        val p1 = p.subtract(BigInteger.ONE) // p - 1
+        println("\t(p - 1): $p1")
+        val e = p1.divide(q) // (p - 1) / q
+        println("\t((p - 1) / q): $e")
+        while (true) {
+            // h in (1, (p-1))
+            val h = BigInteger(p1.bitLength(), random)
+            println("\ttmp(h): $h")
+            if (h in BigInteger.ONE..p1) continue
+            val g = h.modPow(e, p)
+            println("\ttmp(g): $g")
+            if (g == BigInteger.ONE) continue
+            return g
+        }
+    }
+
+    private fun getSpec(
+        L: Int,
+        N: Int,
+        certainty: Int,
+        random: SecureRandom,
+    ): DSAParameterSpec {
+        // https://www.rfc-editor.org/rfc/rfc6979
+        check(setOf(1024, 2048, 3072).contains(L))
+        when (L) {
+            1024 -> check(N == 160)
+            2048 -> check(N == 224 || N == 256)
+            3072 -> check(N == 256)
+        }
+        println("L: $L")
+        println("N: $N")
+        val q: BigInteger
+        while (true) {
+            val tmp = BigInteger(N, certainty, random)
+            if (tmp.isProbablePrime(certainty)) {
+                q = tmp
+                break
+            }
+        }
+        println("q: $q")
+        val p = getP(q = q, L = L, certainty = certainty, random)
+        println("p: $p")
+        return DSAParameterSpec(p, q, getG(p = p, q = q, random))
+    }
+
     private fun create(pin: String): SecretKey {
         val startTime = System.currentTimeMillis().milliseconds // todo
         val hash = hash(pin = pin).base64()
-        val hLen = hash.toByteArray().size
         val random = getSecureRandom()
 //        val bits = 8 * 8 // 64
 //        val bits = 8 * 16 // 128
@@ -90,29 +202,22 @@ internal class EnterViewModel(private val injection: Injection) : AbstractViewMo
         )
         println("create meta: ${System.currentTimeMillis().milliseconds - startTime}")
         injection.files.writeBytes("sym.json", meta.toJson().toString().toByteArray())
-        val primes = 1024 * 1
-//        val primes = 1024 * 2
 //        val primes = Cipher.getMaxAllowedKeyLength("DSA")
-        println("primes: $primes")
-//        val subPrimes = 160 // must be 160 for primes = 1024
-        val subPrimes = 256 // must be 224 or 256 for primes = 2048
         val pair = KeyPairGenerator.getInstance("DSA").let { generator ->
 //            generator.initialize(1024, random)
-            // strength must be from 512 - 4096 and a multiple of 1024 above 1024
+//            L = 1024, N = 160
+//            L = 2048, N = 224
+//            L = 2048, N = 256
+//            L = 3072, N = 256
+//            strength must be from 512 - 4096 and a multiple of 1024 above 1024
+            val primes = 1024 * 1
+//            val primes = 1024 * 2
             generator.initialize(primes, random)
-//            generator.initialize(
-//                DSAParameterSpec(
-//                    BigInteger.probablePrime(primes, random),
-//                    BigInteger.probablePrime(subPrimes, random),
-//                    BigInteger.probablePrime(primes, random),
-//                ),
-//            )
-//            generator.initialize(1024 * 3, random)
-//            generator.initialize(1024 * 4, random)
-//            generator.initialize(2.0.pow(10).toInt(), random)
-//            generator.initialize(2.0.pow(12).toInt(), random)
-//            generator.initialize(2.0.pow(16).toInt(), random)
-//            generator.initialize(2048, random)
+            val subPrimes = 160 // must be 160 for primes = 1024
+//            val subPrimes = 256 // must be 224 or 256 for primes = 2048
+            val certainty = 20
+//            val certainty = 100
+//            generator.initialize(getSpec(L = primes, N = subPrimes, certainty = certainty, random))
             generator.generateKeyPair().also {
                 val private = it.private
                 check(private is DSAPrivateKey)
