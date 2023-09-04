4fa7b27f25b520a0c8cf2254351e0fdc07ac97be
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-04 18:24:49 +0300

    set colors

--- app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/SettingsScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/xfiles/module/enter/SettingsScreen.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,17 +23,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import org.kepocnhh.xfiles.App
 import org.kepocnhh.xfiles.R
+import org.kepocnhh.xfiles.module.app.Colors
 import org.kepocnhh.xfiles.module.app.ColorsType
 import org.kepocnhh.xfiles.module.theme.ThemeViewModel
+import org.kepocnhh.xfiles.util.compose.AnimatedFadeVisibility
 
 @Composable
 internal fun SettingsScreen(onBack: () -> Unit) {
@@ -43,6 +49,79 @@ internal fun SettingsScreen(onBack: () -> Unit) {
         }
         else -> {
             SettingsScreenPortrait()
+        }
+    }
+}
+
+@Composable
+private fun getIcon(colorsType: ColorsType): Int {
+    return when (colorsType) {
+        ColorsType.DARK -> R.drawable.moon
+        ColorsType.LIGHT -> R.drawable.sun
+        ColorsType.AUTO -> if (isSystemInDarkTheme()) {
+            R.drawable.moon
+        } else {
+            R.drawable.sun
+        }
+    }
+}
+
+@Composable
+private fun getText(colorsType: ColorsType): String {
+    return when (colorsType) {
+        ColorsType.DARK -> App.Theme.strings.dark
+        ColorsType.LIGHT -> App.Theme.strings.light
+        ColorsType.AUTO -> App.Theme.strings.auto
+    }
+}
+
+@Composable
+private fun ColorRow(
+    colors: Colors,
+    icon: Int,
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(App.Theme.sizes.xxxl)
+            .background(colors.background)
+            .clickable(onClick = onClick),
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(start = App.Theme.sizes.small)
+                .size(App.Theme.sizes.medium)
+                .align(Alignment.CenterStart),
+            painter = painterResource(id = icon),
+            contentDescription = "colors:row:icon",
+            colorFilter = ColorFilter.tint(colors.foreground),
+        )
+        BasicText(
+            modifier = Modifier.align(Alignment.Center),
+            style = TextStyle(
+                color = colors.foreground,
+                fontSize = 14.sp,
+            ),
+            text = text,
+        )
+        AnimatedFadeVisibility(
+            modifier = Modifier
+                .padding(end = App.Theme.sizes.small)
+                .fillMaxHeight()
+                .align(Alignment.CenterEnd),
+            visible = selected,
+            duration = App.Theme.durations.animation,
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(App.Theme.sizes.small),
+                painter = painterResource(id = R.drawable.check),
+                contentDescription = "colors:row:check",
+                colorFilter = ColorFilter.tint(colors.foreground),
+            )
         }
     }
 }
@@ -64,21 +143,12 @@ private fun SettingsColors() {
                 dialogState.value = true
             }
     ) {
-        val icon = when (theme.colorsType) {
-            ColorsType.DARK -> R.drawable.moon
-            ColorsType.LIGHT -> R.drawable.sun
-            ColorsType.AUTO -> if (isSystemInDarkTheme()) {
-                R.drawable.moon
-            } else {
-                R.drawable.sun
-            }
-        }
         Image(
             modifier = Modifier
                 .padding(start = App.Theme.sizes.small)
                 .size(App.Theme.sizes.medium)
                 .align(Alignment.CenterStart),
-            painter = painterResource(id = icon),
+            painter = painterResource(id = getIcon(theme.colorsType)),
             contentDescription = "colors:icon",
             colorFilter = ColorFilter.tint(App.Theme.colors.foreground),
         )
@@ -99,15 +169,59 @@ private fun SettingsColors() {
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
             ),
-            text = when (theme.colorsType) {
-                ColorsType.DARK -> App.Theme.strings.dark
-                ColorsType.LIGHT -> App.Theme.strings.light
-                ColorsType.AUTO -> App.Theme.strings.auto
-            },
+            text = getText(theme.colorsType),
         )
     }
     if (dialogState.value) {
-        // todo
+        Dialog(
+            onDismissRequest = {
+                dialogState.value = false
+            },
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = App.Theme.colors.background,
+                        shape = RoundedCornerShape(App.Theme.sizes.medium),
+                    )
+                    .padding(
+                        top = App.Theme.sizes.medium,
+                        bottom = App.Theme.sizes.medium,
+                    ),
+            ) {
+                ColorRow(
+                    colors = Colors.Light,
+                    icon = R.drawable.sun,
+                    text = App.Theme.strings.light,
+                    selected = theme.colorsType == ColorsType.LIGHT,
+                    onClick = {
+                        themeViewModel.setColorsType(ColorsType.LIGHT)
+                        dialogState.value = false
+                    }
+                )
+                ColorRow(
+                    colors = Colors.Dark,
+                    icon = R.drawable.moon,
+                    text = App.Theme.strings.dark,
+                    selected = theme.colorsType == ColorsType.DARK,
+                    onClick = {
+                        themeViewModel.setColorsType(ColorsType.DARK)
+                        dialogState.value = false
+                    }
+                )
+                ColorRow(
+                    colors = Colors.Dark,
+                    icon = getIcon(ColorsType.AUTO),
+                    text = App.Theme.strings.auto,
+                    selected = theme.colorsType == ColorsType.AUTO,
+                    onClick = {
+                        themeViewModel.setColorsType(ColorsType.AUTO)
+                        dialogState.value = false
+                    }
+                )
+            }
+        }
     }
 }
 
