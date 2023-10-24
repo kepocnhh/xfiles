22244da8bae3da0f11691c72ec13f91d0c005f89
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-24 18:30:52 +0300

    compose:1.4.3 -> 1.5.3

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,13 +2,13 @@ object Version {
     const val detekt = "1.22.0"
     const val jacoco = "0.8.8"
     const val jvmTarget = "17"
-    const val kotlin = "1.8.10"
+    const val kotlin = "1.9.10"
     const val ktlint = "0.50.0"
 
     object Android {
         const val compileSdk = 34
         const val minSdk = 24
         const val targetSdk = compileSdk
-        const val compose = "1.4.3" // kotlin 1.8.10
+        const val compose = "1.5.3" // kotlin 1.9.10
     }
 }
