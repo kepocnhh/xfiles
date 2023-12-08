c50fa3b606c1e30089c0e4f41647ecdc69e8b49a
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-08 16:21:38 +0300

    0.6.1-29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = namespace
         minSdk = Version.Android.minSdk
         targetSdk = Version.Android.targetSdk
-        versionName = "0.6.0"
-        versionCode = 28
+        versionName = "0.6.1"
+        versionCode = 29
         manifestPlaceholders["appName"] = "@string/app_name"
     }
 
