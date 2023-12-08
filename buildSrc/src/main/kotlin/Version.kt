aaa58b558ba6566f1a062a73f0cfb8875b76379a
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-08 14:40:48 +0300

    up

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,14 +1,14 @@
 object Version {
-    const val detekt = "1.22.0"
-    const val jacoco = "0.8.8"
+    const val detekt = "1.23.1"
+    const val jacoco = "0.8.9"
     const val jvmTarget = "17"
-    const val kotlin = "1.9.10"
+    const val kotlin = "1.9.20"
     const val ktlint = "0.50.0"
 
     object Android {
         const val compileSdk = 34
         const val minSdk = 24
         const val targetSdk = compileSdk
-        const val compose = "1.5.3" // kotlin 1.9.10
+        const val compose = "1.5.4" // kotlin 1.9.20
     }
 }
