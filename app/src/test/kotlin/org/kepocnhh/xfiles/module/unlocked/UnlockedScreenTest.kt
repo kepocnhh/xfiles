fede5c4ee44b8240377b6c71cfe147a8c0b6cd38
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-21 15:13:06 +0300

    UnlockedScreenTest:deleteTest

--- app/src/test/kotlin/org/kepocnhh/xfiles/module/unlocked/UnlockedScreenTest.kt
+++ app/src/test/kotlin/org/kepocnhh/xfiles/module/unlocked/UnlockedScreenTest.kt
@@ -3,6 +3,7 @@ package org.kepocnhh.xfiles.module.unlocked
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -16,7 +17,9 @@ import org.junit.runner.RunWith
 import org.kepocnhh.xfiles.App
 import org.kepocnhh.xfiles.TestActivity
 import org.kepocnhh.xfiles.clearStores
+import org.kepocnhh.xfiles.entity.MockPrivateKey
 import org.kepocnhh.xfiles.entity.MockSecretKey
+import org.kepocnhh.xfiles.entity.mockAsymmetricKey
 import org.kepocnhh.xfiles.entity.mockDataBase
 import org.kepocnhh.xfiles.entity.mockKeyMeta
 import org.kepocnhh.xfiles.entity.mockSecurityServices
@@ -25,10 +28,12 @@ import org.kepocnhh.xfiles.module.app.mockInjection
 import org.kepocnhh.xfiles.module.app.mockThemeState
 import org.kepocnhh.xfiles.provider.MockEncryptedFileProvider
 import org.kepocnhh.xfiles.provider.MockSerializer
+import org.kepocnhh.xfiles.provider.MockTimeProvider
 import org.kepocnhh.xfiles.provider.data.MockLocalDataProvider
 import org.kepocnhh.xfiles.provider.mockPathNames
 import org.kepocnhh.xfiles.provider.security.MockCipherProvider
 import org.kepocnhh.xfiles.provider.security.MockSecurityProvider
+import org.kepocnhh.xfiles.provider.security.MockSignatureProvider
 import org.kepocnhh.xfiles.setContent
 import org.kepocnhh.xfiles.setInjection
 import org.kepocnhh.xfiles.waitOne
@@ -36,6 +41,7 @@ import org.robolectric.RobolectricTestRunner
 import java.util.UUID
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
+import kotlin.time.Duration.Companion.seconds
 
 @RunWith(RobolectricTestRunner::class)
 internal class UnlockedScreenTest {
@@ -332,11 +338,104 @@ internal class UnlockedScreenTest {
                 },
             )
         }
+        val isTitle = hasContentDescription("UnlockedScreen:item:$id:title")
+        rule.waitOne(isTitle and hasText(title))
         val isSecret = hasContentDescription("UnlockedScreen:secret")
         rule.onNode(isSecret).assertDoesNotExist()
         val isButton = SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Button)
         val isShow = hasContentDescription("UnlockedScreen:item:$id:show")
         rule.onNode(isButton and isShow).performClick()
         rule.waitOne(isSecret and hasText(secret))
+    }
+
+    @Test(timeout = 2_000)
+    fun deleteTest() {
+        val id = UUID.randomUUID()
+        val title = "UnlockedScreenTest:showTest:title"
+        val secret = "UnlockedScreenTest:showTest:secret"
+        val initDataBase = mockDataBase(
+            secrets = mapOf(id to (title to secret)),
+        )
+        val initDataBaseDecrypted = "initDataBase:decrypted".toByteArray()
+        val initDataBaseEncrypted = "initDataBase:encrypted".toByteArray()
+        check(initDataBase.secrets.size == 1)
+        val dataBaseRef = AtomicReference(initDataBaseEncrypted)
+        val updated = 128.seconds
+        val editedDataBase = initDataBase.copy(
+            updated = updated,
+            secrets = emptyMap(),
+        )
+        check(editedDataBase.secrets.isEmpty())
+        val editedDataBaseDecrypted = "editedDataBase:decrypted".toByteArray()
+        val editedDataBaseEncrypted = "editedDataBase:encrypted".toByteArray()
+        val editedDataBaseSignature = "editedDataBase:signature".toByteArray()
+        check(initDataBase.updated.inWholeMilliseconds < editedDataBase.updated.inWholeMilliseconds)
+        val symmetric = mockKeyMeta()
+        val symmetricDecrypted = "symmetric:decrypted".toByteArray()
+        val asymmetric = mockAsymmetricKey()
+        val asymmetricDecrypted = "asymmetric:decrypted".toByteArray()
+        val pathNames = mockPathNames()
+        val privateKey = MockPrivateKey("UnlockedScreenTest:deleteTest:privateKey".toByteArray())
+        val key = MockSecretKey("UnlockedScreenTest:deleteTest:secretKey".toByteArray())
+        val injection = mockInjection(
+            local = MockLocalDataProvider(services = mockSecurityServices()),
+            pathNames = pathNames,
+            security = {
+                MockSecurityProvider(
+                    cipher = MockCipherProvider(
+                        values = listOf(
+                            Triple(initDataBaseEncrypted, initDataBaseDecrypted, key),
+                            Triple(editedDataBaseEncrypted, editedDataBaseDecrypted, key),
+                            Triple(asymmetric.privateEncrypted, privateKey.encoded, key),
+                        ),
+                    ),
+                    signature = MockSignatureProvider(
+                        signatures = mapOf(editedDataBaseDecrypted to editedDataBaseSignature),
+                    ),
+                )
+            },
+            encrypted = mockEncrypted(
+                files = MockEncryptedFileProvider(
+                    inputs = mapOf(
+                        pathNames.symmetric to symmetricDecrypted,
+                        pathNames.asymmetric to asymmetricDecrypted,
+                    ),
+                    refs = mapOf(
+                        pathNames.dataBase to dataBaseRef,
+                    ),
+                ),
+            ),
+            serializer = MockSerializer(
+                values = mapOf(
+                    symmetric to symmetricDecrypted,
+                    asymmetric to asymmetricDecrypted,
+                    initDataBase to initDataBaseDecrypted,
+                    editedDataBase to editedDataBaseDecrypted,
+                ),
+            ),
+            time = MockTimeProvider(now = updated),
+        )
+        val strings = rule.setContent(injection) {
+            UnlockedScreen(
+                key = key,
+                broadcast = {
+                    error("Illegal state!")
+                },
+            )
+        }
+        val isEmpty = hasContentDescription("UnlockedScreen:empty")
+        rule.onNode(isEmpty).assertDoesNotExist()
+        val isTitle = hasContentDescription("UnlockedScreen:item:$id:title")
+        rule.waitOne(isTitle and hasText(title))
+        val dialogText = String.format(strings.unlocked.deleteItem, title)
+        rule.onNode(hasText(dialogText)).assertDoesNotExist()
+        val isButton = SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Button)
+        val isDelete = hasContentDescription("UnlockedScreen:item:$id:delete")
+        rule.onNode(isButton and isDelete).performClick()
+        rule.onNode(hasText(dialogText)).assertIsDisplayed()
+        rule.waitOne(isTitle and hasText(title))
+        rule.onNode(hasText(strings.yes)).performClick()
+        rule.waitOne(isEmpty)
+        rule.onNode(isTitle and hasText(title)).assertDoesNotExist()
     }
 }
